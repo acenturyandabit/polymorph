c924e23c4ddbfc186cfafee19641a32353a2a5bb
acenturyandabit <steeven.liu2@gmail.com>
2020-06-07 15:06:58 +1000

    auto pre-push commit

--- build.js
+++ build.js
@@ -35,7 +35,7 @@ let { execSync } = require("child_process");
     }
     console.log("minifying....");
     await compressor.minify({
-        compressor: 'yui',
+        compressor: 'noCompress',
         input: files,
         output: 'deploy.js',
     });
