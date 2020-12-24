92e78cb6396c0ea6308fbf5d5d95af199cfd09d1
acenturyandabit <steeven.liu2@gmail.com>
2020-12-24 11:35:17 +1100

    codequal: naming cleanup

--- core_modules/ui/core.dialog.js
+++ core_modules/ui/core.dialog.js
@@ -130,7 +130,7 @@ function _dialogManager(userSettings) {
                     e.parentElement.appendChild(closeButton);
                     //only bind escape close to dialogs that have a closeButton
                     window.addEventListener("keydown", (e) => {
-                        if (e.key == "Escape" && prediv.style.display!="none") {
+                        if (e.key == "Escape" && prediv.style.display != "none") {
                             closeButton.click();
                         }
                     });
@@ -180,18 +180,18 @@ polymorph_core.dialog.div.classList.add("dialog");
 polymorph_core.dialog.div = dialogManager.checkDialogs(polymorph_core.dialog.div)[0];
 polymorph_core.dialog.innerDialog = polymorph_core.dialog.div.querySelector(".innerDialog");
 document.body.appendChild(polymorph_core.dialog.div)
-//polymorph_core.dialog.currentBaseOperator
+    //polymorph_core.dialog.currentBaseOperator
 
 //Register a dialog to a calling rect. Rect calls this when the settings cog is clicked.
-polymorph_core.dialog.prompt = function (dialog, closeCB) {
+polymorph_core.dialog.prompt = function(dialog, closeCallback) {
     //instantly show a dialog with contents 'dialog'.
     //use HTMLwrap to create a dom element or otherwise.
     while (polymorph_core.dialog.innerDialog.children.length > 2) polymorph_core.dialog.innerDialog.children[2].remove();
     polymorph_core.dialog.innerDialog.appendChild(dialog);
     polymorph_core.dialog.div.style.display = "block";
-    polymorph_core.dialog.closeCB = closeCB;
+    polymorph_core.dialog.closeCB = closeCallback;
 }
-polymorph_core.dialog.div.querySelector(".cb").addEventListener("click", function () {
+polymorph_core.dialog.div.querySelector(".cb").addEventListener("click", function() {
     if (polymorph_core.dialog.closeCB) {
         try {
             polymorph_core.dialog.closeCB(polymorph_core.dialog.innerDialog);
